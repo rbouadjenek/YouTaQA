175ea92a1e9d91bd5748c6a554f476b08719e4a5
Mohamed Reda Bouadjenek <rbouadjenek@gmail.com>
2020-04-16 17:51:52 +1000

    adding copyright

--- DeepQA/preprocessing/parse.py
+++ DeepQA/preprocessing/parse.py
@@ -1,34 +1,19 @@
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#  Copyright (c) 2020. Reda Bouadjenek, Deakin University
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at:
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS,
-#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#     See the License for the specific language governing permissions and
-#      limitations under the License.
-#
-#
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at:
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS,
-#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#     See the License for the specific language governing permissions and
-#      limitations under the License.
-#
-#
+#  Copyright (c) 2020. Reda Bouadjenek, Deakin University                      +
+#  reda.bouadjenek@deakin.edu.au                                               +
+#                                                                              +
+#  Licensed under the Apache License, Version 2.0 (the "License");             +
+#   you may not use this file except in compliance with the License.           +
+#    You may obtain a copy of the License at:                                  +
+#                                                                              +
+#                 http://www.apache.org/licenses/LICENSE-2.0                   +
+#                                                                              +
+#    Unless required by applicable law or agreed to in writing, software       +
+#    distributed under the License is distributed on an "AS IS" BASIS,         +
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  +
+#    See the License for the specific language governing permissions and       +
+#    limitations under the License.                                            +
+#                                                                              +
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 import getopt
@@ -144,5 +129,3 @@ if __name__ == "__main__":
     The main function.
     """
     main(sys.argv[1:])
-    # process("/home/reda/NetBeansProjects/DeepQA/enwiki-20200401/enwiki-20200401-pages-articles-multistream.xml",
-    #        "/home/reda/NetBeansProjects/DeepQA/enwiki-20200401/")
