7e376ffd4989ab9284e06c65d290dde9a5a8fb8e
AGABI Younes <younes.agabi03@gmail.com>
2020-04-29 18:38:19 +0100

    Update test.py

--- DeepQA/indexing/test.py
+++ DeepQA/indexing/test.py
@@ -13,7 +13,7 @@ from xml.dom import minidom
 from indexer import Indexer, Section, Document, Searcher
 inputQuery = ""
 while inputQuery != "exit":
-    inputQuery = input("Type your request, otherwise tape 'exit' to exit\n")
+    inputQuery = input("Enter your request, otherwise tape 'exit' to exit\n")
     result = Searcher.search(os.getcwd() + "/index", inputQuery)
     if result.is_empty() is True:
         print("No result found")
