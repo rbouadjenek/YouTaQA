f95482d745f781438284778f64df93cae68d8e9f
AGABI Younes <younes.agabi03@gmail.com>
2020-05-04 18:59:45 +0100

    Update test.py

--- DeepQA/indexing/test.py
+++ DeepQA/indexing/test.py
@@ -15,23 +15,33 @@ from cosine_similarity import CosineSimilarity
 inputQuery = ""
 CosineSimilarity = CosineSimilarity()
 
-while inputQuery != "exit":
-    inputQuery = input("Enter your request, otherwise tape 'exit' to exit\n")
-    if inputQuery == "exit":
-        break
-    result = Searcher().search(os.getcwd() + "/index", inputQuery)
-    if result.is_empty() is True:
-        print("No result found")
-    else:
-        print("#" * 100)
-        print("#" * 100)
-        content = ""
-        for hit in result:
-            content = hit["content_section"]
-            if content != "":
-                break
-        print(content)
-        print("#" * 50)
-        CosSim = CosineSimilarity.similarity(inputQuery, content)
-        print(CosSim)
-        print("#" * 100)
+# while inputQuery != "exit":
+#     inputQuery = input("Enter your request, otherwise tape 'exit' to exit\n")
+#     if inputQuery == "exit":
+#         break
+#     result = Searcher().search("/Users/younesagabi/Desktop/index", inputQuery)
+#     if result.is_empty() is True:
+#         print("No result found")
+#     else:
+#         print("#" * 100)
+#         print("#" * 100)
+#         content = ""
+#         for hit in result:
+#             content = hit["content_section"]
+#             if content != "":
+#                 break
+#         print(content)
+#         print("#" * 50)
+#         CosSim = CosineSimilarity.similarity(inputQuery, content)
+#         print(CosSim)
+#         print("#" * 100)
+i=0
+if index.exists_in("/Users/younesagabi/Desktop/DeepQA/DeepQA/indexing/index") is True:
+    index=index.open_dir("/Users/younesagabi/Desktop/DeepQA/DeepQA/indexing/index")
+    all_docs = index.searcher().documents()
+    for doc in all_docs:
+        i+=1
+        print(i)
+    print(i)
+else :
+    print("Error")
