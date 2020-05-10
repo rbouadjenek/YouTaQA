c2dcce84ef7e5623ab1de1b7a191ca7e049668c7
AGABI Younes <younes.agabi03@gmail.com>
2020-05-10 17:07:57 +0100

    Add Lucune indexer

--- DeepQA/indexing/indexer.py
+++ DeepQA/indexing/indexer.py
@@ -5,14 +5,21 @@ import os.path
 import sys
 import wiki_extractor
 from tqdm import tqdm
-from whoosh import index, qparser, scoring
-from whoosh.analysis import *
-from whoosh.fields import Schema, TEXT, ID
-from whoosh.qparser import MultifieldParser
+import lucene
+from org.apache.lucene.analysis.en import EnglishAnalyzer
+from org.apache.lucene.index import IndexWriterConfig, IndexWriter, DirectoryReader
+from org.apache.lucene.codecs.simpletext import SimpleTextCodec
+from org.apache.lucene.store import FSDirectory
+from java.nio.file import Path, Paths
+from org.apache.lucene.document import Document, StringField, Field, TextField
+from org.apache.lucene.search import IndexSearcher
+from org.apache.lucene.queryparser.classic import QueryParser
+from org.apache.lucene.analysis import StopwordAnalyzerBase, CharArraySet
+from java.util import List, Arrays
 from xml.dom import minidom
 
 
-class Document:
+class WikiDocument:
     """
     A class to represent a wikipedia document as extracted from the page tag in the wikipedia dump.
     """
@@ -77,100 +84,6 @@ class Indexer:
         the methods of indexing documents.
     """
 
-    CustomStopList = frozenset(["a", "a's", "able",
-                                "about", "above", "according", "accordingly", "across",
-                                "actually", "after", "afterwards", "again", "against", "ain't",
-                                "all", "allow", "allows", "almost", "alone", "along",
-                                "already", "also", "although", "always", "am", "among",
-                                "amongst", "an", "and", "another", "any", "anybody", "anyhow",
-                                "anyone", "anything", "anyway", "anyways", "anywhere", "apart",
-                                "appear", "appreciate", "appropriate", "are", "aren't",
-                                "around", "as", "aside", "ask", "asking", "associated", "at",
-                                "available", "away", "awfully", "b", "be", "became", "because",
-                                "become", "becomes", "becoming", "been", "before",
-                                "beforehand", "behind", "being", "believe", "below", "beside",
-                                "besides", "best", "better", "between", "beyond", "both",
-                                "brief", "but", "by", "c", "c'mon", "c's", "came", "can",
-                                "can't", "cannot", "cant", "cause", "causes", "certain",
-                                "certainly", "changes", "clearly", "co", "com", "come",
-                                "comes", "concerning", "consequently", "consider",
-                                "considering", "contain", "containing", "contains",
-                                "corresponding", "could", "couldn't", "course", "currently",
-                                "d", "definitely", "described", "despite", "did", "didn't",
-                                "different", "do", "does", "doesn't", "doing", "don't", "done",
-                                "down", "downwards", "during", "e", "each", "edu", "eg",
-                                "eight", "either", "else", "elsewhere", "enough", "entirely",
-                                "especially", "et", "etc", "even", "ever", "every",
-                                "everybody", "everyone", "everything", "everywhere", "ex",
-                                "exactly", "example", "except", "f", "far", "few", "fifth",
-                                "first", "five", "followed", "following", "follows", "for",
-                                "former", "formerly", "forth", "four", "from", "further",
-                                "furthermore", "g", "get", "gets", "getting", "given", "gives",
-                                "go", "goes", "going", "gone", "got", "gotten", "greetings",
-                                "h", "had", "hadn't", "happens", "hardly", "has", "hasn't",
-                                "have", "haven't", "having", "he", "he's", "hello", "help",
-                                "hence", "her", "here", "here's", "hereafter", "hereby",
-                                "herein", "hereupon", "hers", "herself", "hi", "him",
-                                "himself", "his", "hither", "hopefully", "how", "howbeit",
-                                "however", "i", "i'd", "i'll", "i'm", "i've", "ie", "if",
-                                "ignored", "immediate", "in", "inasmuch", "inc", "indeed",
-                                "indicate", "indicated", "indicates", "inner", "insofar",
-                                "instead", "into", "inward", "is", "isn't", "it", "it'd",
-                                "it'll", "it's", "its", "itself", "j", "just", "k", "keep",
-                                "keeps", "kept", "know", "knows", "known", "l", "last",
-                                "lately", "later", "latter", "latterly", "least", "less",
-                                "lest", "let", "let's", "like", "liked", "likely", "little",
-                                "look", "looking", "looks", "ltd", "m", "mainly", "many",
-                                "may", "maybe", "me", "mean", "meanwhile", "merely", "might",
-                                "more", "moreover", "most", "mostly", "much", "must", "my",
-                                "myself", "n", "name", "namely", "nd", "near", "nearly",
-                                "necessary", "need", "needs", "neither", "never",
-                                "nevertheless", "new", "next", "nine", "no", "nobody", "non",
-                                "none", "noone", "nor", "normally", "not", "nothing", "novel",
-                                "now", "nowhere", "o", "obviously", "of", "off", "often", "oh",
-                                "ok", "okay", "old", "on", "once", "one", "ones", "only",
-                                "onto", "or", "other", "others", "otherwise", "ought", "our",
-                                "ours", "ourselves", "out", "outside", "over", "overall",
-                                "own", "p", "particular", "particularly", "per", "perhaps",
-                                "placed", "please", "plus", "possible", "presumably",
-                                "probably", "provides", "q", "que", "quite", "qv", "r",
-                                "rather", "rd", "re", "really", "reasonably", "regarding",
-                                "regardless", "regards", "relatively", "respectively", "right",
-                                "s", "said", "same", "saw", "say", "saying", "says", "second",
-                                "secondly", "see", "seeing", "seem", "seemed", "seeming",
-                                "seems", "seen", "self", "selves", "sensible", "sent",
-                                "serious", "seriously", "seven", "several", "shall", "she",
-                                "should", "shouldn't", "since", "six", "so", "some",
-                                "somebody", "somehow", "someone", "something", "sometime",
-                                "sometimes", "somewhat", "somewhere", "soon", "sorry",
-                                "specified", "specify", "specifying", "still", "sub", "such",
-                                "sup", "sure", "t", "t's", "take", "taken", "tell", "tends",
-                                "th", "than", "thank", "thanks", "thanx", "that", "that's",
-                                "thats", "the", "their", "theirs", "them", "themselves",
-                                "then", "thence", "there", "there's", "thereafter", "thereby",
-                                "therefore", "therein", "theres", "thereupon", "these", "they",
-                                "they'd", "they'll", "they're", "they've", "think", "third",
-                                "this", "thorough", "thoroughly", "those", "though", "three",
-                                "through", "throughout", "thru", "thus", "to", "together",
-                                "too", "took", "toward", "towards", "tried", "tries", "truly",
-                                "try", "trying", "twice", "two", "u", "un", "under",
-                                "unfortunately", "unless", "unlikely", "until", "unto", "up",
-                                "upon", "us", "use", "used", "useful", "uses", "using",
-                                "usually", "uucp", "v", "value", "various", "very", "via",
-                                "viz", "vs", "w", "want", "wants", "was", "wasn't", "way",
-                                "we", "we'd", "we'll", "we're", "we've", "welcome", "well",
-                                "went", "were", "weren't", "what", "what's", "whatever",
-                                "when", "whence", "whenever", "where", "where's", "whereafter",
-                                "whereas", "whereby", "wherein", "whereupon", "wherever",
-                                "whether", "which", "while", "whither", "who", "who's",
-                                "whoever", "whole", "whom", "whose", "why", "will", "willing",
-                                "wish", "with", "within", "without", "won't", "wonder",
-                                "would", "would", "wouldn't", "x", "y", "yes", "yet", "you",
-                                "you'd", "you'll", "you're", "you've", "your", "yours"])
-
-    analyzer = RegexTokenizer() | LowercaseFilter() | IntraWordFilter(splitwords=True,
-                                                                      splitnums=True) | StopFilter(stoplist=CustomStopList, lang="en") | StemFilter()
-
     def __init__(self, index_dir):
         """
 
@@ -179,20 +92,13 @@ class Indexer:
         self.indexDir = index_dir
         if not os.path.exists(index_dir):
             os.mkdir(index_dir)
+        lucene.initVM(vmargs=['-Djava.awt.headless=true'])
+        self.ENGLISH_STOP_WORDS_SET = CharArraySet.unmodifiableSet(CharArraySet(Arrays.asList(["a", "is", "able"]), False))
+        self.analyzer = EnglishAnalyzer(self.ENGLISH_STOP_WORDS_SET)
+        self.conf = IndexWriterConfig(self.analyzer)
+        self.conf.setUseCompoundFile(False)
+        self.directory = FSDirectory.open(Paths.get("index_text/"))
 
-        # Define a Schema for the index
-        self.mySchema = Schema(id_article=ID(stored=True),
-                               title_article=TEXT(
-                                   analyzer=Indexer.analyzer, stored=True),
-                               id_section=ID(stored=True),
-                               title_section=TEXT(
-                                   analyzer=Indexer.analyzer, stored=True),
-                               content_section=TEXT(analyzer=Indexer.analyzer, stored=True))
-        # create the index
-        self.myIndex = index.create_in(index_dir, self.mySchema)
-        # batch writing is faster.
-        self.writer = self.myIndex.writer(
-            procs=4, limitmb=1024)
 
     def index_folder(self, folder2index):
         """
@@ -208,7 +114,7 @@ class Indexer:
         with tqdm(total=num_lines) as pbar:
             for file in files:
                 pbar.update(1)
-                doc = Document(file)  # this parse the wikipedia page
+                doc = WikiDocument(file)  # this parse the wikipedia page
                 self.index_document(doc)  # this indexes the wikipedia page
         print("\n==> Please wait ...\n")
         self.close()
@@ -221,17 +127,21 @@ class Indexer:
         """
         # Method that indexes documents
         i = 0
+        self.writer = IndexWriter(self.directory, self.conf)
+        docu = Document()
         for section in doc.sections:
-            self.writer.add_document(id_article=doc.id,
-                                     title_article=doc.title,
-                                     id_section=str(doc.id) + "_" + str(i),
-                                     title_section=section.title,
-                                     content_section=section.text)
+            docu.add(StringField("id_article", doc.id, Field.Store.YES))
+            docu.add(TextField("title_article", doc.title, Field.Store.YES))
+            docu.add(StringField("id_section", str(doc.id) + "_" + str(i), Field.Store.YES))
+            docu.add(TextField("title_section", section.title, Field.Store.YES))
+            docu.add(TextField("content_section", section.text, Field.Store.YES))
+            self.writer.addDocument(docu)
+
             i += 1
 
     def close(self):
         # close the index
-        self.writer.commit(optimize=True)
+        self.writer.close()
 
 
 class Searcher:
