f83268b4a271967b5c76388b25e5bd329bbaa179
Asma Tidafi <AsmaTidafi@users.noreply.github.com>
2020-05-21 16:46:03 +0100

    Update qrels.py

--- DeepQA/indexing/qrels.py
+++ DeepQA/indexing/qrels.py
@@ -3,8 +3,16 @@ import json
 import os.path
 import sys
 from tqdm import tqdm
+import lucene
+from indexer import Document
 from search import Searcher
+from wiki_doc import Section
+from java.nio.file import Path
 from org.apache.lucene.search.similarities import *
+from org.apache.lucene.document import Document, Field
+from org.apache.lucene.index import (DirectoryReader, IndexWriter,
+                                     IndexWriterConfig)
+from similarity import Similarity
 
 
 class qrels:
@@ -12,27 +20,29 @@ class qrels:
     A class to create the qrels of the questions for SQUAD dataset.
     """
 
-    def __init__(self, index_dir):
-        self.searchObject = Searcher(index_dir)
+    def __init__(self):
+        super().__init__()
 
-    def get_id_section(self, pair):
+    def get_id_section(index_dir, input_query):
         """
         This is the function that returns the id of the passage that is similar to the context.
         :param index_dir: the folder where the dataset index is stored.
         :param input_query: the query that represents the context.
         :return:
         """
-        result = self.searchObject.simpleSearch(pair, BM25Similarity())
-        id = None
+        searchObject = Searcher(index_dir)
+        result = searchObject.simpleSearch(input_query, BM25Similarity())
         for i in range(len(result)):
-            hitDoc = self.searchObject.searcher.doc(result[i].doc)
+            hitDoc = searchObject.searcher.doc(result[i].doc)
             id = hitDoc.get("id_section")
             content = hitDoc.get("content_section")
             if id != "":
                 break
+        searchObject.reader.close()
+        searchObject.directory.close()
         return id, content
 
-    def process(self, input_file, output_dir):
+    def process(input_file, index_dir, output_dir):
         """
         This is the main function that creates the qrels file.
         :param input_file: the file to process.
@@ -52,17 +62,17 @@ class qrels:
             with open(input_file, encoding="utf-8") as json_file:
                 data = json.load(json_file)
                 for p in data['data']:
-
                     for par in p['paragraphs']:
                         pbar.update(1)
-                        psg_id, content = self.get_id_section(
-                            (p['title'], par["context"]))
-                        # print("Content: "+content+"\n")
-                        similarity = round(len(set(par["context"]) & set(content)) / len(set(par["context"])), 2)
+                        psg_id, content = qrels.get_id_section(
+                            index_dir, par["context"])
                         for q in par["qas"]:
                             qst_id = q["id"]
-                            output_file.write(
-                                qst_id + " 0 " + psg_id + " 1 " + str(similarity) + "\n")
+                            if q["is_impossible"] == false
+                                similarity = round(len(set(par["context"]) & set(
+                                    content)) / len(set(par["context"])), 2)
+                                output_file.write(
+                                    qst_id + " 0 " + psg_id + " 1 " + str(similarity) + "\n")
                 print("==> Qrels successfully created.\n")
 
 
@@ -98,7 +108,7 @@ def main(argv):
                 print(output_dir + ' does not exist.')
                 print("\n")
                 sys.exit(2)
-        elif opt in "-d":
+        elif opt in ("-d"):
             index_dir = arg
             if not os.path.isdir(index_dir):
                 print(index_dir + ' does not exist.')
@@ -106,8 +116,7 @@ def main(argv):
                 sys.exit(2)
 
     if input_file != '' and output_dir != '' and index_dir != '':
-        qrelevance = qrels(index_dir)
-        qrelevance.process(input_file, output_dir)
+        qrels.process(input_file, index_dir, output_dir)
     else:
         print('qrels.py -i <input_file> -d <index_dir> -o <output_folder>')
         sys.exit(2)
