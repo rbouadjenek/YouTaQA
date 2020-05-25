15c1fd4ce255c0ea190ae5ff6993136ef6418121
AGABI Younes <younes.agabi03@gmail.com>
2020-05-25 01:08:27 +0100

    Update results_generator.py

--- DeepQA/indexing/results_generator.py
+++ DeepQA/indexing/results_generator.py
@@ -30,13 +30,13 @@ class ResultsGenerator:
         output_file_4 = open(
             output_dir + "/results_BM25_4.txt", 'a+', encoding="utf-8")
         output_file_5 = open(
-            output_dir + "/results_TDF_1.txt", 'a+', encoding="utf-8")
+            output_dir + "/results_VSM_1.txt", 'a+', encoding="utf-8")
         output_file_6 = open(
-            output_dir + "/results_TDF_2.txt", 'a+', encoding="utf-8")
+            output_dir + "/results_VSM_2.txt", 'a+', encoding="utf-8")
         output_file_7 = open(
-            output_dir + "/results_TDF_3.txt", 'a+', encoding="utf-8")
+            output_dir + "/results_VSM_3.txt", 'a+', encoding="utf-8")
         output_file_8 = open(
-            output_dir + "/results_TDF_4.txt", 'a+', encoding="utf-8")
+            output_dir + "/results_VSM_4.txt", 'a+', encoding="utf-8")
 
         num_lines = 0
         with open(input_file, encoding="utf-8") as json_file:
@@ -62,28 +62,27 @@ class ResultsGenerator:
                                 question_title_content_s_BM25 = self.searcher.pairSearch(
                                     [title, q["question"]], BM25Similarity())
                                 id_question_title_content_s_BM25 = self.get_id_section(question_title_content_s_BM25)
-
+                                
                                 question_content_m_BM25 = self.searcher.multiFieldsSearch(
                                     q["question"], BM25Similarity())
                                 id_question_content_m_BM25 = self.get_id_section(question_content_m_BM25)
-
+                                
                                 question_title_content_m_BM25 = self.searcher.multiFieldsPairSearch(
                                     [title, q["question"]], BM25Similarity())
                                 id_question_title_content_m_BM25 = self.get_id_section(question_title_content_m_BM25)
 
-
                                 question_content_s_TDF = self.searcher.simpleSearch(
                                     q["question"], ClassicSimilarity())
                                 id_question_content_s_TDF = self.get_id_section(question_content_s_TDF)
-
+                                
                                 question_title_content_s_TDF = self.searcher.pairSearch(
                                     [title, q["question"]], ClassicSimilarity())
                                 id_question_title_content_s_TDF = self.get_id_section(question_title_content_s_TDF)
-
+                                
                                 question_content_m_TDF = self.searcher.multiFieldsSearch(
                                     q["question"], ClassicSimilarity())
                                 id_question_content_m_TDF = self.get_id_section(question_content_m_TDF)
-
+                                
                                 question_title_content_m_TDF = self.searcher.multiFieldsPairSearch(
                                     [title, q["question"]], ClassicSimilarity())
                                 id_question_title_content_m_TDF = self.get_id_section(question_title_content_m_TDF)
@@ -100,7 +99,6 @@ class ResultsGenerator:
                                 for i in range(len(question_title_content_m_BM25)):
                                     output_file_4.write(
                                         q["id"] + " Q0 " + str(id_question_title_content_m_BM25[i]) + " " + str(i+1) + " " + str(question_title_content_m_BM25[i].score) + " STANDARD\n")
-
                                 for i in range(len(question_content_s_TDF)):
                                     output_file_5.write(
                                         q["id"] + " Q0 " + str(id_question_content_s_TDF[i]) + " " + str(i+1) + " " + str(question_content_s_TDF[i].score) + " STANDARD\n")
