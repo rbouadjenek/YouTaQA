719da598426ff0724303b5ea4373c3a54f3d02a5
AGABI Younes <younes.agabi03@gmail.com>
2020-06-27 23:26:20 +0100

    Create run_squad.py

---
+++ DeepLearning/Answer Extraction/run_squad.py
@@ -0,0 +1,15 @@
+from transformers import BertTokenizer, BertForQuestionAnswering
+import torch
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+model = BertForQuestionAnswering.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
+text=""
+while text != 'exit':
+    text = input('Enter your text:\n')
+    question = input('Enter your question:\n')
+    encoding = tokenizer.encode_plus(question, text)
+    input_ids, token_type_ids = encoding["input_ids"], encoding["token_type_ids"]
+    start_scores, end_scores = model(torch.tensor([input_ids]), token_type_ids=torch.tensor([token_type_ids]))
+    all_tokens = tokenizer.convert_ids_to_tokens(input_ids)
+    answer = ' '.join(all_tokens[torch.argmax(start_scores) : torch.argmax(end_scores)+1])
+    print(answer)
